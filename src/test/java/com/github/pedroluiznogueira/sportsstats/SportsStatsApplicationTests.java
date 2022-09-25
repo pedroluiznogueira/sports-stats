c945f3299b0721b202308a5a340dbaa9f2bdc291
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-09-25 20:48:30 -0300

    couchbase test containers configurations for integration/functional tests
    - added bucket's credentials to Couchbase
    
    - added bootstrap properties to AbstractTests

--- src/test/java/com/github/pedroluiznogueira/sportsstats/SportsStatsApplicationTests.java
+++ src/test/java/com/github/pedroluiznogueira/sportsstats/SportsStatsApplicationTests.java
@@ -2,12 +2,17 @@ package com.github.pedroluiznogueira.sportsstats;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.couchbase.CouchbaseContainer;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-class SportsStatsApplicationTests {
+public class SportsStatsApplicationTests extends AbstractTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(getCouchbaseContainer())
+				.extracting(CouchbaseContainer::isRunning)
+				.isEqualTo(Boolean.TRUE);
 	}
-
 }
