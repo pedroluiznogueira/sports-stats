dab00aa8e03cb2c15a82d9405aa732782ef65c53
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-09-25 20:00:13 -0300

    initial commit

---
+++ src/main/java/com/github/pedroluiznogueira/sportsstats/SportsStatsApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.sportsstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsStatsApplication.class, args);
+	}
+
+}
